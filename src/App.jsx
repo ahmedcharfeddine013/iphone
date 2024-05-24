2a2080d79dad2d9bea65cda9f050a5b8961609b8
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-05-24 03:30:29 +0100

    carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Highlights from './components/Highlights'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className='bg-black'>
       <Navbar />
       <Hero />
+      <Highlights />
     </div>
   )
 }
